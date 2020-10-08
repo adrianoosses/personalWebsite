6bc316bca20a0122f430752f9b87e34412a84703
aosses <adrianoosses9@gmail.com>
2020-10-08 18:21:45 +0200

    Botones

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function despliegaMenu(){
+    if(document.getElementById('luzVerde').style.opacity == 0){
+        document.getElementById('luzVerde').style.opacity=1;
+    }else{
+        document.getElementById('luzVerde').style.opacity=0;
+    }
+}
